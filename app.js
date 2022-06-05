f0b768aefec361da0fd35f51b955e18d0c8a4002
Kagweitis <kagwegeorge82@gmail.com>
2022-06-05 16:11:35 +0300

    app.js change

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ const app = Vue.createApp({
         }
     },
     methods:{ 
-        getUser() {
-            this.firstName = 'Adam'
-            this.lastName='freeman'
-            this.email= 'adam@gmail.com'
-           this.gender='female'
-           this.picture= 'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250'
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            
+            //console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName= results[0].name.last
+            this.email= results[0].email
+           this.gender= results[0].gender
+           this.picture= results[0].picture.large
         },
      },
 })
